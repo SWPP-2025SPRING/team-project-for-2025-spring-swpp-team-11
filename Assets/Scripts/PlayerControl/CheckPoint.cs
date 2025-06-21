f39958414356f66de2c7e2d26fa2b7a7e8af709e
Yi Daeun <dawnayi2002@gmail.com>
2025-06-21 15:33:12 +0900

    fix: respawn 시 guideline 재생성 (checkpoint file)

--- Assets/Scripts/PlayerControl/CheckPoint.cs
+++ Assets/Scripts/PlayerControl/CheckPoint.cs
@@ -3,11 +3,18 @@ using UnityEngine;
 
 public class CheckPoint : MonoBehaviour
 {
+    [Tooltip("이 지점에 리스폰되면 이 인덱스부터 가이드라인을 표시")]
+    public int startCheckpointIndex = 0;
+
+    [Tooltip("가이드라인 스크립트 참조")]
+    public GuidanceLine.GuidanceLine guide;
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
             other.GetComponent<PlayerBehavior>().respawnPos = transform;
+            guide.ResetGuidance(startCheckpointIndex);
         }
     }
 }
